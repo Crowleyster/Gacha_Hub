23297c2669870663fc6365b80d6c45373f222d17
Alan <alan@example.com>
2026-03-18 17:13:09 -0300

    UI: Clear Home page content shell

--- app/page.js
+++ app/page.js
@@ -1,15 +1,7 @@
-import NewsBentoGrid from '@/components/NewsBentoGrid';
-import PromotionalBanner from '@/components/PromotionalBanner';
-import ActiveCodes from '@/components/ActiveCodes';
-import EventsTimeline from '@/components/EventsTimeline';
-
 export default function Home() {
   return (
-    <div className="col-span-full space-y-12 md:space-y-16 pb-12 font-sans">
-      <NewsBentoGrid />
-      <PromotionalBanner />
-      <ActiveCodes />
-      <EventsTimeline />
+    <div className="col-span-full">
+      {/* Home shell */}
     </div>
   );
 }
