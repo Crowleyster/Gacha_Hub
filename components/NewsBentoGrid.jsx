4a278120b9797281e4b3de7750f449d23a5eeacb
Alan <alan@example.com>
2026-03-19 22:39:01 -0300

    refactor: implement dynamic data-driven layout for NewsBentoGrid

--- components/NewsBentoGrid.jsx
+++ components/NewsBentoGrid.jsx
@@ -57,23 +57,26 @@ export default function NewsBentoGrid() {
         }
     ];
 
+    // Layout Configuration for the 6 Positions
+    const BENTO_LAYOUT = [
+        "col-start-1 col-span-4 row-start-2 row-span-2 sm:col-span-8 md:col-span-12 lg:col-span-6 lg:row-start-2 lg:row-span-2", // Card-1 (Hero)
+        "col-start-1 col-span-4 row-start-5 row-span-1 xs:row-start-4 sm:col-start-5 sm:col-span-4 sm:row-start-4 md:col-start-7 md:col-span-6 lg:col-start-1 lg:col-span-6 lg:row-start-4 lg:row-span-1", // Card-2
+        "col-start-1 col-span-2 row-start-4 row-span-1 xs:row-start-6 xs:row-span-2 sm:row-start-4 sm:row-span-2 md:col-span-3 lg:col-start-7 lg:col-span-3 lg:row-start-3 lg:row-span-2", // Card-3
+        "col-start-3 col-span-2 row-start-4 row-span-1 xs:row-start-5 xs:row-span-2 sm:row-start-4 sm:row-span-2 md:col-start-4 md:col-span-3 md:row-start-4 md:row-span-2 lg:col-start-10 lg:col-span-3 lg:row-start-2 lg:row-span-2", // Card-4
+        "col-start-1 col-span-2 row-start-6 row-span-1 xs:row-start-5 sm:col-start-5 sm:row-start-5 sm:row-span-1 md:col-start-7 md:col-span-3 md:row-start-5 md:row-span-1 lg:col-start-7 lg:col-span-3 lg:row-start-2 lg:row-span-1", // Card-5
+        "col-start-3 col-span-2 row-start-6 row-span-1 xs:row-start-7 sm:col-start-7 sm:row-start-5 md:col-start-10 md:col-span-3 lg:col-start-10 lg:col-span-3 lg:row-start-4 lg:row-span-1" // Card-6
+    ];
+
     return (
         <section className="
             grid w-full font-sans
             /* Parent Grid Config (7 Breakpoints) */
-            /* XXS (Base < 540px) */
             grid-cols-4 gap-4 grid-rows-[29px_repeat(5,240px)_46px]
-            /* XS (540px - 767px) */
             xs:grid-rows-[29px_repeat(6,240px)_46px]
-            /* S (768px - 1023px) */
             sm:grid-cols-8 sm:gap-6 sm:grid-rows-[46px_repeat(4,240px)]
-            /* M (1024px - 1279px) */
             md:grid-cols-12 md:grid-rows-[46px_repeat(4,240px)]
-            /* L (1280px - 1535px) */
             lg:grid-rows-[46px_repeat(3,240px)]
-            /* XL (1536px - 1919px) */
             xl:grid-rows-[46px_repeat(3,240px)]
-            /* XXL (>= 1920px) */
             2xl:grid-rows-[46px_repeat(3,240px)]
             grid-auto-rows-[240px]
         ">
@@ -88,7 +91,6 @@ export default function NewsBentoGrid() {
                 <h2 className="text-heading text-text-default-default font-sans">
                     Noticias Destacadas
                 </h2>
-                {/* Botón visible solo en Tablet/Desktop en Fila 1 */}
                 <Link
                     href="/noticias"
                     className="hidden sm:flex items-center gap-2 px-4 py-2 rounded-lg bg-background-secondary hover:bg-background-secondary-hover text-text-default-default transition-colors text-body-small-strong"
@@ -98,74 +100,18 @@ export default function NewsBentoGrid() {
                 </Link>
             </div>
 
-            {/* 2. 6 Tarjetas (Priority Order: Card-1 to Card-6) */}
+            {/* 2. Dynamic Bento Content */}
+            {newsData.slice(0, 6).map((news, index) => (
+                <div key={news.id} className={BENTO_LAYOUT[index]}>
+                    <NewsCard 
+                        {...news} 
+                        isHero={index === 0}
+                        isSmall={index >= 2} 
+                    />
+                </div>
+            ))}
 
-            {/* Card-1 (Hero) */}
-            <div className="
-                col-start-1 col-span-4 row-start-2 row-span-2
-                sm:col-span-8
-                md:col-span-12
-                lg:col-span-6 lg:row-start-2 lg:row-span-2
-            ">
-                <NewsCard {...newsData[0]} />
-            </div>
-
-            {/* Card-2 */}
-            <div className="
-                col-start-1 col-span-4 row-start-5 row-span-1
-                xs:row-start-4
-                sm:col-start-5 sm:col-span-4 sm:row-start-4
-                md:col-start-7 md:col-span-6
-                lg:col-start-1 lg:col-span-6 lg:row-start-4 lg:row-span-1
-            ">
-                <NewsCard {...newsData[1]} />
-            </div>
-
-            {/* Card-3 */}
-            <div className="
-                col-start-1 col-span-2 row-start-4 row-span-1
-                xs:row-start-6 xs:row-span-2
-                sm:row-start-4 sm:row-span-2
-                md:col-span-3
-                lg:col-start-7 lg:col-span-3 lg:row-start-3 lg:row-span-2
-            ">
-                <NewsCard {...newsData[2]} isSmall={true} />
-            </div>
-
-            {/* Card-4 */}
-            <div className="
-                col-start-3 col-span-2 row-start-4 row-span-1
-                xs:row-start-5 xs:row-span-2
-                sm:row-start-4 sm:row-span-2
-                md:col-start-4 md:col-span-3 md:row-start-4 md:row-span-2
-                lg:col-start-10 lg:col-span-3 lg:row-start-2 lg:row-span-2
-            ">
-                <NewsCard {...newsData[3]} isSmall={true} />
-            </div>
-
-            {/* Card-5 */}
-            <div className="
-                col-start-1 col-span-2 row-start-6 row-span-1
-                xs:row-start-5
-                sm:col-start-5 sm:row-start-5 sm:row-span-1
-                md:col-start-7 md:col-span-3 md:row-start-5 md:row-span-1
-                lg:col-start-7 lg:col-span-3 lg:row-start-2 lg:row-span-1
-            ">
-                <NewsCard {...newsData[4]} isSmall={true} />
-            </div>
-
-            {/* Card-6 */}
-            <div className="
-                col-start-3 col-span-2 row-start-6 row-span-1
-                xs:row-start-7
-                sm:col-start-7 sm:row-start-5
-                md:col-start-10 md:col-span-3
-                lg:col-start-10 lg:col-span-3 lg:row-start-4 lg:row-span-1
-            ">
-                <NewsCard {...newsData[5]} isSmall={true} />
-            </div>
-
-            {/* 3. Button (Mobile only at the end) */}
+            {/* 3. Mobile Button (at the end) */}
             <Link
                 href="/noticias"
                 className="
