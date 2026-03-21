fad12dcf8d6817df4b4a0fba9ed5f5102dc04673
Alan Godoy <al.godoy.94@gmail.com>
2026-03-21 16:32:32 -0300

    Actualizar layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
-import ClientLayout from './ClientLayout';
+import ClientLayout from '@/components/ClientLayout';
 
 const inter = Inter({ subsets: ['latin'] });
 
