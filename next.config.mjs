bc7b79325ac0878de7b07b45c7138fa5376d1bb8
Alan Godoy <al.godoy.94@gmail.com>
2026-03-20 21:01:49 -0300

    Actualizar next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-
-  const nextConfig = {
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'gamespace.com' },
-      { protocol: 'https', hostname: 'play-lh.googleusercontent.com' },
-    ],
     unoptimized: true,
   },
-};
-
 };
 
 export default nextConfig;
