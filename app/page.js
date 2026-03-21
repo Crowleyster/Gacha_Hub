90f69241fa2c1ffa0cf57a72ec74806accced708
Alan Godoy <al.godoy.94@gmail.com>
2026-03-21 02:22:33 -0300

    Actualizar page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import NewsBentoGrid from "@/components/NewsBentoGrid";
 import ActiveCodes from "@/components/ActiveCodes";
+import EventsTimeline from '@/components/EventsTimeline';
 
 export default function Home() {
   return (
     <div className="col-span-full space-y-16">
       <NewsBentoGrid />
       <ActiveCodes />
+      <EventsTimeline />
     </div>
   );
 }
